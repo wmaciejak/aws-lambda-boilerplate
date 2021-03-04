fdff3d5ac8666d8db70ab5d1aace06d3dea275d8
Wojciech Maciejak <wojciech.maciejak@gmail.com>
2021-03-04 10:32:50 +0100

    Provide root gateway integration

--- gateway_deployment.tf
+++ gateway_deployment.tf
@@ -1,6 +1,7 @@
 resource "aws_api_gateway_deployment" "hello_world" {
    depends_on = [
      aws_api_gateway_integration.lambda_hello_world_integration,
+     aws_api_gateway_integration.lambda_root,
    ]
 
    rest_api_id = aws_api_gateway_rest_api.hello_world.id
--- gateway_resource.tf
+++ gateway_resource.tf
@@ -10,3 +10,10 @@ resource "aws_api_gateway_method" "proxy" {
    http_method   = "ANY"
    authorization = "NONE"
 }
+
+resource "aws_api_gateway_method" "proxy_root" {
+   rest_api_id   = aws_api_gateway_rest_api.hello_world.id
+   resource_id   = aws_api_gateway_rest_api.hello_world.root_resource_id
+   http_method   = "ANY"
+   authorization = "NONE"
+}
