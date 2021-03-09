483828ded3346f47168879aa6928b6ddd9e7f408
Wojciech Maciejak <wojciech.maciejak@gmail.com>
2021-03-09 11:29:21 +0100

    Provide function to automatically compress lambda

--- lambda.tf
+++ lambda.tf
@@ -1,6 +1,11 @@
+data "archive_file" "lambda_zip" {
+    type        = "zip"
+    source_file  = "source/function.rb"
+    output_path = "function.rb.zip"
+}
 resource "aws_lambda_function" "hello_world" {
   function_name = "HelloWorld"
-  filename      = "source/function.rb.zip"
+  filename      = "function.rb.zip"
   handler       = "function.lambda_handler"
   runtime       = "ruby2.7"
   role          = aws_iam_role.iam_role.arn
