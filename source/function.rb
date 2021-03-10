a5b87203816658431bcbfbb7cea7feeffd3d2c6c
Tomasz Kapłon <tomasz.kaplon@monterail.com>
2021-03-10 12:36:46 +0100

    show how to read params, body and headers

--- source/function.rb
+++ source/function.rb
@@ -3,8 +3,20 @@
 require "json"
 
 def lambda_handler(event:, context:)
+  response_body = {
+    # Hash of request headers, e.g. { "Content-Type"=>"application/json", "User-Agent"=>"PostmanRuntime/7.26.2" }
+    headers: event["headers"],
+
+    # String with request body, e.g. "{\"param1\": 1}"
+    body: event["body"],
+
+    # Hash of query string params, e.g. for "a=1&b[]=2&b[]=3&c[d]=4&e=5&e=6"
+    # it returns {"a"=>"1", "b[]"=>["2", "3"], "c[d]"=>"4", "e"=>["5", "6"]}
+    querystring_params: event["queryStringParameters"],
+  }
+
   {
     statusCode: 200,
-    body: JSON.generate(event: event, context: context),
+    body: JSON.generate(response_body),
   }
 end
