75b6b9501a257db69682092ab55404b1aca841b7
Tomasz Kapłon <tomasz.kaplon@monterail.com>
2021-03-10 15:08:24 +0100

    fixes after merge

--- source/lambda2.rb
+++ source/lambda2.rb
@@ -3,13 +3,23 @@
 require "json"
 
 def lambda_handler(event:, context:)
-  puts "\n\n\n\n"
-  puts event.to_json
-  puts "\n\n\n\n"
-  puts context.to_json
-  puts "\n\n\n\n"
+  response_body = {
+    # Hash of request headers, e.g. { "Content-Type"=>"application/json", "User-Agent"=>"PostmanRuntime/7.26.2" }
+    headers: event.fetch("headers"),
+
+    # String with request body, e.g. "{\"param1\": 1}"
+    body: event.fetch("body"),
+
+    # Hash of query string params, e.g. for "a=1&b[]=2&b[]=3&c[d]=4&e=5&e=6"
+    # it returns {"a"=>"1", "b[]"=>["2", "3"], "c[d]"=>"4", "e"=>["5", "6"]}
+    querystring_params: event.fetch("queryStringParameters"),
+
+    # Hash of ENV variables
+    env: ENV.to_h,
+  }
+
   {
     statusCode: 200,
-    body: JSON.generate("Lambda 2 say 'OK'"),
+    body: response_body,
   }
 end
