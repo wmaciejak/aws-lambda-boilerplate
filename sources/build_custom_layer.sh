e859a7e7fb33f07c133e0804a8f9853a20759350
Tomasz Kapłon <tomasz.kaplon@monterail.com>
2021-03-18 10:27:16 +0100

    Fix linux setup (permissions, networks)

--- sources/build_custom_layer.sh
+++ sources/build_custom_layer.sh
@@ -1,12 +1,11 @@
-rm -rf gems
+#!/bin/sh
 
-docker run --rm -v $PWD:/var/layer \
-           -w /var/layer \
-           amazon/aws-sam-cli-build-image-ruby2.7 \
-           bundle install --path=gems
-
-mv gems/ruby/* gems/ \
-  && rm -rf gems/2.7.0/cache \
-  && rm -rf gems/ruby
-
-rm -rf .bundle
+docker run --rm -v $PWD/$1:/var/layer -w /var/layer amazon/aws-sam-cli-build-image-ruby2.7 bash -c $'
+rm -rf gems \
+&& bundle config set --local path \'gems\' \
+&& bundle install \
+&& mv gems/ruby/* gems/ \
+&& rm -rf gems/2.7.0/cache \
+&& rm -rf gems/ruby \
+&& rm -rf .bundle
+'
