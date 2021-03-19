27298a320e74117ada0df403210222692e8557ba
Tomasz Kapłon <tomasz.kaplon@monterail.com>
2021-03-19 09:12:15 +0100

    merge Artyom's changes

--- sources/build_custom_layer.sh
+++ sources/build_custom_layer.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
-docker run --rm -v $PWD/$1:/var/layer -w /var/layer amazon/aws-sam-cli-build-image-ruby2.7 bash -c $'
-rm -rf gems \
-&& bundle config set --local path \'gems\' \
-&& bundle install \
-&& mv gems/ruby/* gems/ \
-&& rm -rf gems/2.7.0/cache \
-&& rm -rf gems/ruby \
-&& rm -rf .bundle
-'
+image=amazon/aws-sam-cli-build-image-ruby2.7
+module=$1
+
+if [ -z "$module" ]; then
+  echo "Usage: $0 <module>"
+  exit 1
+fi
+
+docker run --rm -v $PWD/$module:/var/layer -w /var/layer $image sh -c "
+  set -x \
+  && rm -rf gems \
+  && bundle config set --local path 'gems' \
+  && bundle install \
+  && mv gems/ruby/* gems/ \
+  && rm -rf gems/2.7.0/cache \
+  && rm -rf gems/ruby \
+  && rm -rf .bundle \
+  && chown -R $(id -u):$(id -g) gems/
+"
