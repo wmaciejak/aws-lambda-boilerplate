f32f07bd42465bda65d38405733816aa34facd13
Artyom Nosov <chip@unixstyle.ru>
2021-03-24 12:34:16 +0300

    workspace support has been added.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,23 @@
-output "lambda_1_url" {
-  value = "http://localhost:4566/restapis/${aws_api_gateway_rest_api.hello_world.id}/dev/_user_request_${aws_api_gateway_resource.lambda_1.path}"
+output "localstack_lambda_1_url" {
+  value = "${var.localstack_url}/restapis/${aws_api_gateway_rest_api.hello_world.id}/dev/_user_request_${aws_api_gateway_resource.lambda_1.path}"
 }
 
-output "lambda_2_url" {
-  value = "http://localhost:4566/restapis/${aws_api_gateway_rest_api.hello_world.id}/dev/_user_request_${aws_api_gateway_resource.lambda_2.path}"
+output "localstack_lambda_2_url" {
+  value = "${var.localstack_url}/restapis/${aws_api_gateway_rest_api.hello_world.id}/dev/_user_request_${aws_api_gateway_resource.lambda_2.path}"
+}
+
+output "localstack_lambda_3_url" {
+  value = "${var.localstack_url}/restapis/${aws_api_gateway_rest_api.hello_world.id}/dev/_user_request_${aws_api_gateway_resource.lambda_3.path}"
+}
+
+output "aws_lambda_1_url" {
+  value = "https://${aws_api_gateway_rest_api.hello_world.id}.execute-api.${var.aws_region}.amazonaws.com/${var.api_gateway_deployment_stage_name}${aws_api_gateway_resource.lambda_1.path}"
+}
+
+output "aws_lambda_2_url" {
+  value = "https://${aws_api_gateway_rest_api.hello_world.id}.execute-api.${var.aws_region}.amazonaws.com/${var.api_gateway_deployment_stage_name}${aws_api_gateway_resource.lambda_2.path}"
+}
+
+output "aws_lambda_3_url" {
+  value = "https://${aws_api_gateway_rest_api.hello_world.id}.execute-api.${var.aws_region}.amazonaws.com/${var.api_gateway_deployment_stage_name}${aws_api_gateway_resource.lambda_3.path}"
 }
